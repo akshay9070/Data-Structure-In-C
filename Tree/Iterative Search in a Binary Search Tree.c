f2e9c6c7a64145747b62a035fc325d2cb147201e
akshay9070 <86796423+akshay9070@users.noreply.github.com>
2021-07-18 16:59:25 +0530

    Iterative Search in a Binary Search Tree

---
+++ Tree/Iterative Search in a Binary Search Tree.c
@@ -0,0 +1,79 @@
+/*Binary Search Tree:
+         5
+        / \ 
+        4  6
+       / \
+       2  3
+*/
+
+
+#include<stdio.h>
+#include<malloc.h>
+
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data)
+{
+    struct node* n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+
+struct node* search(struct node* root, int key)
+{
+    while(root!=NULL)
+    {
+        if(root->data==key)
+        {
+            return root;
+        }
+        else if(root->data>key)
+        {
+            return search(root->left,key);
+        }
+        else
+        {
+            return search(root->right,key);
+        }
+    }
+    return NULL;
+}
+
+
+
+int main()
+{
+    int s;
+    printf("Enter To Search IN BST \n");
+    scanf("%d",&s);
+    struct node* p=createNode(5);
+    struct node* p1=createNode(4);
+    struct node* p2=createNode(2);
+    struct node* p3=createNode(3);
+    struct node* p4=createNode(6);
+
+
+    p->left=p1;
+    p->right=p4;
+    p1->left=p2;
+    p1->right=p3;
+
+    struct node* n=search(p,s);
+    if(n==NULL)
+    {
+        printf("Element Not Present In BST\n");
+    }
+    else{
+        printf("Element Present In BST %d",n->data);
+    }
+
+
+}
